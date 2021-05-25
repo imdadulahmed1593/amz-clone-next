561848af3382b6ece9835f45cfa9e8169aec1d2e
IMDAD <ahmedimdad1593@gmail.com>
2021-05-25 23:57:22 +0600

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,13 @@ const Header = () => {
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         {/* logo  */}
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-          <img
+          <Image
             onClick={() => router.push("/")}
             src="https:links.papareact.com/f90"
-            width={100}
-            height={20}
-            className="cursor-pointer mx-5 object-contain"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
           />
         </div>
 
