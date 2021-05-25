8572e22c622ac8de8614840fc70bf1781a9a89c0
IMDAD <ahmedimdad1593@gmail.com>
2021-05-25 23:38:51 +0600

    links created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,13 +14,25 @@ const Banner = () => {
         interval={5000}
       >
         <div>
-          <img loading="lazy" src="https:links.papareact.com/gi1" alt="" />
+          <img
+            loading="lazy"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/kindle/content/GTM/Editorial/0504-AMZN-GNBC-GatewayHero-1500x600_v5._CB669739807_.jpg"
+            alt=""
+          />
         </div>
         <div>
-          <img loading="lazy" src="https:links.papareact.com/6ff" alt="" />
+          <img
+            loading="lazy"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/launch/gateway/TheUndergroundRailroad/UGRR_S1_GWBleedingHero_ENG_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB669781769_.jpg"
+            alt=""
+          />
         </div>
         <div>
-          <img loading="lazy" src="https:links.papareact.com/7ma" alt="" />
+          <img
+            loading="lazy"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/AmazonMusic/2021/Marketing/SWSpringDeal_DMUX-4280/Gateway/DV2/UK-EN_030821_SpringSitewide_ACQ_GW_Hero_D_1500x600_CV69._CB656397523_.jpg"
+            alt=""
+          />
         </div>
       </Carousel>
     </div>
